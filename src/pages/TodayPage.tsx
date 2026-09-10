e9cf34ba00a0359016c5a8d138b68a36194721e6
JLHammer <jens@lindberghammer.dk>
2026-09-10 08:50:41 +0200

    feat: load and render today's events

--- src/pages/TodayPage.tsx
+++ src/pages/TodayPage.tsx
@@ -1,3 +1,24 @@
+import { ContentWrapper } from "../components/partials/ContentWrapper";
+import { EventList } from "../components/partials/EventList";
+import { useFetch } from "../hooks/useFetch";
+import { entriesWithYear, todayUrl } from "../api/history";
+import { historyResponseSchema } from "../types/history";
+
 export const TodayPage = () => {
-  return <h1>TodayPage</h1>;
+  const { data, error, loading } = useFetch(todayUrl(), {
+    schema: historyResponseSchema,
+  });
+
+  return (
+    <ContentWrapper
+      label="On this day"
+      description="What happened on this day - Here you can enter a specific date to only get events that happened on this date"
+    >
+      <EventList
+        entries={data && entriesWithYear(data.data.Events)}
+        loading={loading}
+        error={error}
+      />
+    </ContentWrapper>
+  );
 };
